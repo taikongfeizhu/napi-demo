a70310abb99a38d097079b1417ee9786066e58a3
huangjian <huangjian@bytedance.com>
2023-11-18 21:43:28 +0800

    feat(domainMaps):build demo test
    Change-Id: I32fcc52acd2f5b4fac1e55f419e81fabd3617037

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,13 @@
 import test from 'ava'
 
-import { plus100 } from '../index'
+import { plus100, sub100 } from '../index'
 
-test('sync function from native code', (t) => {
+test('plus100 from native code', (t) => {
   const fixture = 42
   t.is(plus100(fixture), fixture + 100)
+})
+
+test('sub100 from native code', (t) => {
+  const fixture = 200
+  t.is(sub100(fixture), fixture - 100)
 })
